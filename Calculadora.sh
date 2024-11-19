6b9e6ce7bcdf54f5d8f595717a2bf5a17c301a2e
Mariana Oliveira <mariannarodrigues.0402@gmail.com>
2024-11-19 22:33:18 +0000

    Add files via upload

---
+++ Calculadora.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+echo "Bem vindo à calculadora"
+echo "Escolha uma operação" 
+echo "1) Adição"
+echo "2) Subtração"
+echo "3) Multiplicação"
+echo "4) Divisão"
+read -p "Digite a opção (1/2/3/4): " operação 
+
+read -p "Digite o primeiro número: "num1"
+read -p "Digite o segunte número: "num2"
+
+if operação == "+"
+resultado = "num1 + num2"
+print(resultado)
+
+
+elif operação == "-": 
+resultado == "num1" - "num2" 
+print(resultado) 
+
+elif operação == "*" 
+resultado == "num1 * num2" 
+print(resultado) 
+
+elif operação == "//":
+resultado == "num1 // num2" 
+print(resultado) 
+
+else operaçao == "num1 < num2"
+print("Operaçao invalida")
+
+else operaçao == "num2 ==0" 
+print("Operaçao invalida"
+
+
